b8d41453ce9cfd2096c319ea423b5fe0dd6b2121
moeenaslam <moeenkashmiri@gmail.com>
2022-08-19 17:40:55 +0500

    Profile changes

--- src/main/java/com/example/libarymgmt/LibaryMgmtApplication.java
+++ src/main/java/com/example/libarymgmt/LibaryMgmtApplication.java
@@ -1,13 +1,34 @@
 package com.example.libarymgmt;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class LibaryMgmtApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(LibaryMgmtApplication.class, args);
+    new SpringApplicationBuilder(LibaryMgmtApplication.class).profiles("dev","prod").run(args);
   }
 
+}
+
+
+@Component
+class MyRunner implements CommandLineRunner {
+
+  @Autowired
+  private Environment environment;
+
+  @Override
+  public void run(String... args) throws Exception {
+    System.out.println("Active profiles: " +
+        Arrays.toString(environment.getActiveProfiles()));
+  }
 }
